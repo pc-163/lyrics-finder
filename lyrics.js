9ad3aeb20cdb37459b31787af4a59623d8b08350
Prakash Chand <rajputpc163@hotmail.com>
2022-07-06 09:34:55 +0530

    update files

--- lyrics.js
+++ lyrics.js
@@ -3,8 +3,10 @@
 //https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search
 //http://127.0.0.1:5500/apiwork/api.html
 
-function searchSong() {
 
+
+function searchSong() {
+    document.getElementById('loader').style.display = 'block'; //loader visible
     //let song = document.getElementById('search').value;
     let artist = document.getElementById('search').value;
     const url = `https://api.lyrics.ovh/suggest/${artist}`;
@@ -13,16 +15,20 @@ function searchSong() {
         .then(res => res.json())
         .then(jsonData => {
             let addmusic = '';
+            document.getElementById('loader').style.display = 'none';  //loader hide
             jsonData.data.map((values) => {
                 //console.log('title', values);
 
-                addmusic += `
-                         <ul class="list_data set-list" id= "${values.id}">
-                            <li class="_sno track"><img src="${values.artist.picture}"><span>${values.title}</span></li>
-                            <li class="_sno artist"><span>${values.artist.name}</span></li>
-                            <li class="_sno album"><span>${values.album.title}</span></li>
-                            <li class="_sno duration"><button class="btn btn-warning rounded-0" onclick="lyrics('${values.title}','${values.artist.name}')">Lyrics</button></li>
-                        </ul>`;
+              
+                    addmusic += `
+                    <ul class="list_data set-list" id= "${values.id}">
+                       <li class="_sno track"><img src="${values.artist.picture}"><span>${values.title}</span></li>
+                       <li class="_sno artist"><span>${values.artist.name}</span></li>
+                       <li class="_sno album"><span>${values.album.title}</span></li>
+                       <li class="_sno duration"><button class="btn btn-warning rounded-0" onclick="lyrics('${values.title}','${values.artist.name}')">Lyrics</button></li>
+                   </ul>`;
+                
+              
             });
             document.getElementById("getmusic").innerHTML = addmusic;
             //document.getElementById('search').value = '';
@@ -33,20 +39,28 @@ function searchSong() {
 
 function lyrics(title, artist) {
 
+    document.getElementById('getmusic').style.display = 'none'; //music list hide
+    document.getElementById('heading-hide').style.display = 'none'; //Lyrics titles hide
+    document.getElementById('loader').style.display = 'block'; //loader visible
+    document.getElementById('lyrics-hide').style.display = 'flex'; //lyrics title show
+
     const fetchLyricsurl = `https://api.lyrics.ovh/v1/${artist}/${title}`;
 
         fetch(fetchLyricsurl)
             .then(res => res.json())
             .then(lyricsData => {
                 //console.log('lyricsData', lyricsData);
-                if (lyricsData.lyrics.length === 0) {
+                if (lyricsData.lyrics.length == 0) {
                     alert('No Lyrics found');
                 } else {
+
+                    document.getElementById('loader').style.display = 'none'; //loader hide
+
                     document.getElementById('getmusic').style.display = 'none';
-                    document.getElementById('heading-hide').style.display = 'none';
-                    document.getElementById('lyrics-hide').style.display = 'flex';
+                    // document.getElementById('heading-hide').style.display = 'none';
+                    // document.getElementById('lyrics-hide').style.display = 'flex';
                     document.getElementById('setLyrics').style.display = 'block';
-                    let x = lyricsData.lyrics.trim();//replace(/\n/g);
+                    let x = lyricsData.lyrics.trim(); //replace(/\n/g);
                     document.getElementById('addLyrics').innerHTML =  `<p>${x}</p>`;
                 }
                 
@@ -60,6 +74,6 @@ function clickme(){
     document.getElementById('lyrics-hide').style.display = 'none';
     document.getElementById('heading-hide').style.display = 'flex';
     document.getElementById('getmusic').style.display = 'block';
-
-
 }
+
+
